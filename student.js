6ce6801d8e77e0abb86cd06d7b7c1923200d3bd8
NguyenBaoPhuc0394 <=>
2024-04-19 12:25:40 +0700

    update sidebar

--- student.js
+++ student.js
@@ -3,3 +3,15 @@ const sidebar = document.querySelector("#toggle-btn")
 sidebar.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("expand"); 
 });  
+
+
+const sidebarItem = document.querySelectorAll(".sidebar-item")
+function active(event){
+    sidebarItem.forEach(item => {
+        item.classList.remove('active')
+    })
+    event.currentTarget.classList.add('active');
+}
+sidebarItem.forEach(item => {
+    item.addEventListener('click',active);
+})
